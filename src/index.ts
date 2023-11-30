8f42fa1072a158008166efdddf1b70b33cc6f2a3
Asher Beck <117310101+Asherbeck10@users.noreply.github.com>
2023-11-30 09:00:52 +0000

    parsing data

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,10 @@
 import { CsvFileReader } from './CsvFileReader';
+import { MatchResult } from './MatchResult';
+
 
 const reader = new CsvFileReader('football.csv');
 reader.read();
 
-enum MatchResult {
-  HomeWin = 'H',
-  AwayWin = 'A',
-  Draw = 'D',
-}
 let manUnitWins = 0;
 for (let match of reader.data) {
   if (match[1] === 'Man United' && match[5] === MatchResult.HomeWin) {
